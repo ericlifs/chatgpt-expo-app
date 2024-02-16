3f42994af137973ba5d096f1f03ae0ffd31957c5
Eric Lifs <lifszyc.eric@gmail.com>
2024-02-16 18:09:41 -0300

    feat: add functionality to settings screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,9 +9,9 @@ export default function RootLayout() {
     <RootSiblingParent>
       <GestureHandlerRootView style={{ flex: 1 }}>
         <Drawer initialRouteName="chat">
-          <Drawer.Screen name="chat" options={{ headerShown: true, title: 'Chat' }} />
-          <Drawer.Screen name="whisper" options={{ headerShown: true, title: 'Voice to text' }} />
-          <Drawer.Screen name="settings" options={{ headerShown: true, title: 'Settings' }} />
+          <Drawer.Screen name="chat" options={{ title: 'Chat' }} />
+          <Drawer.Screen name="whisper" options={{ title: 'Voice to text' }} />
+          <Drawer.Screen name="settings" options={{ title: 'Settings' }} />
         </Drawer>
       </GestureHandlerRootView>
     </RootSiblingParent>
--- app/settings.tsx
+++ app/settings.tsx
@@ -1,11 +1,45 @@
-import { Text, View } from 'react-native';
+import { router } from 'expo-router';
+import { useState } from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
+import Toast from 'react-native-root-toast';
+
+import { storage } from '~/storage/mmkv';
 
 export default function SettingsScreen() {
+  const initialApiKey = storage.getString('apiKey') || '';
+  const [apiKey, setApiKey] = useState(initialApiKey);
+
+  const saveApiKeyToStorage = () => {
+    storage.set('apiKey', apiKey);
+
+    Toast.show('API Key stored correctly', {
+      position: Toast.positions.TOP,
+      duration: Toast.durations.LONG,
+    });
+
+    router.navigate('/chat');
+  };
+
   return (
-    <>
-      <View className="flex-1 bg-zinc-800 justify-center items-center">
-        <Text className="text-neutral-100">This is the settings screen</Text>
-      </View>
-    </>
+    <View className="flex flex-1 flex-col items-center space-y-4 w-full px-6 py-10">
+      <Text className="mb-4" numberOfLines={1}>
+        Current API Key: {initialApiKey || '-'}
+      </Text>
+      <TextInput
+        value={apiKey}
+        onChangeText={setApiKey}
+        className="mb-4 bg-white border-black border-2 px-4 py-3 w-full rounded-xl text-black placeholder:text-gray-500"
+        placeholder="Enter your API Key"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <TouchableOpacity
+        disabled={!apiKey}
+        className="w-full bg-black disabled:bg-gray-300 py-3 rounded-xl"
+        onPress={saveApiKeyToStorage}>
+        <Text className="text-white text-center">Save API Key</Text>
+      </TouchableOpacity>
+    </View>
   );
 }
