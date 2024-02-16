58e4c81333c87100bc34b46069c338f652671bd4
Eric Lifs <lifszyc.eric@gmail.com>
2024-02-16 18:16:10 -0300

    feat: add react-query dependency

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,24 @@
 import '../global.css';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Drawer } from 'expo-router/drawer';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { RootSiblingParent } from 'react-native-root-siblings';
 
+const queryClient = new QueryClient();
+
 export default function RootLayout() {
   return (
     <RootSiblingParent>
-      <GestureHandlerRootView style={{ flex: 1 }}>
-        <Drawer initialRouteName="chat">
-          <Drawer.Screen name="chat" options={{ title: 'Chat' }} />
-          <Drawer.Screen name="whisper" options={{ title: 'Voice to text' }} />
-          <Drawer.Screen name="settings" options={{ title: 'Settings' }} />
-        </Drawer>
-      </GestureHandlerRootView>
+      <QueryClientProvider client={queryClient}>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+          <Drawer initialRouteName="chat">
+            <Drawer.Screen name="chat" options={{ title: 'Chat' }} />
+            <Drawer.Screen name="whisper" options={{ title: 'Voice to text' }} />
+            <Drawer.Screen name="settings" options={{ title: 'Settings' }} />
+          </Drawer>
+        </GestureHandlerRootView>
+      </QueryClientProvider>
     </RootSiblingParent>
   );
 }
