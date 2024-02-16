9a298951b1a11896d4e5216f05871d3dc9d2ed3c
Eric Lifs <lifszyc.eric@gmail.com>
2024-02-16 11:30:47 -0300

    feat: install toast dependency

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,15 +2,18 @@ import '../global.css';
 
 import { Drawer } from 'expo-router/drawer';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { RootSiblingParent } from 'react-native-root-siblings';
 
 export default function RootLayout() {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <Drawer initialRouteName="chat">
-        <Drawer.Screen name="chat" options={{ headerShown: true, title: 'Chat' }} />
-        <Drawer.Screen name="whisper" options={{ headerShown: true, title: 'Voice to text' }} />
-        <Drawer.Screen name="settings" options={{ headerShown: true, title: 'Settings' }} />
-      </Drawer>
-    </GestureHandlerRootView>
+    <RootSiblingParent>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <Drawer initialRouteName="chat">
+          <Drawer.Screen name="chat" options={{ headerShown: true, title: 'Chat' }} />
+          <Drawer.Screen name="whisper" options={{ headerShown: true, title: 'Voice to text' }} />
+          <Drawer.Screen name="settings" options={{ headerShown: true, title: 'Settings' }} />
+        </Drawer>
+      </GestureHandlerRootView>
+    </RootSiblingParent>
   );
 }
